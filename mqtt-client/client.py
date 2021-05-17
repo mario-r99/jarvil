89a812dbbc984cc29c5cf8edb8259ff41af7178a
Mario <mario.srx99@gmail.com>
2021-05-17 15:16:22 +0200

    Added local connection

--- mqtt-client/client.py
+++ mqtt-client/client.py
@@ -1,11 +1,14 @@
 import paho.mqtt.client as mqtt
 
+# Broker definition
+host = "jarvil_mqtt-broker_1"
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("test")
+    client.subscribe("#")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -15,7 +18,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("192.168.178.17", 1883, 60)
+client.connect(host)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
