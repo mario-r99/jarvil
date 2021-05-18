cf885fa46b745c070288114561fcd8d594d6d46c
Mario <mario.srx99@gmail.com>
2021-05-19 00:27:21 +0200

    Added favicon

--- time-slot-booking/app.py
+++ time-slot-booking/app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+bookings = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', bookings=bookings)
