eae3c6537ca68c81ab4b2bcc15ee0d76441d771f
Mario <mario.srx99@gmail.com>
2021-05-19 01:39:53 +0200

    Added checkboxes

--- time-slot-booking/app.py
+++ time-slot-booking/app.py
@@ -1,19 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 bookings = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
+    [10,10,9,3], # Monday
+    [10,0,9,3],  # Tuesday
+    [10,10,9,3], # Wednesday
+    [1,10,9,3],  # Thursday
+    [10,7,9,3], # Friday
+    [10,10,9,3], # Saturday
+    [10,10,9,3]  # Sunday
 ]
 
 @app.route('/')
