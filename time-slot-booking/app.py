b6334ffc10c638195940333ce6df614fa463ed3e
Mario <mario.srx99@gmail.com>
2021-05-19 23:41:13 +0200

    Added table format

--- time-slot-booking/app.py
+++ time-slot-booking/app.py
@@ -23,5 +23,15 @@ def home():
             return redirect(url_for('home'))
         else:
             flash('Invalid input. Look below for more information.', 'danger')
-            return redirect(url_for('home'))
     return render_template('home.html', bookings=bookings, form=form)
+
+def format_table(form):
+    return [
+        [form.m1.data, form.m2.data, form.m3.data, form.m4.data],
+        [form.t1.data, form.t2.data, form.t3.data, form.t4.data],
+        [form.w1.data, form.w2.data, form.w3.data, form.w4.data],
+        [form.th1.data, form.th2.data, form.th3.data, form.th4.data],
+        [form.f1.data, form.f2.data, form.f3.data, form.f4.data],
+        [form.s1.data, form.s2.data, form.s3.data, form.s4.data],
+        [form.su1.data, form.su2.data, form.su3.data, form.su4.data],
+    ]
--- time-slot-booking/forms.py
+++ time-slot-booking/forms.py
@@ -3,7 +3,40 @@ from wtforms import StringField, SubmitField, BooleanField
 from wtforms.validators import Length
 
 class BookingForm(FlaskForm):
-    booked = BooleanField()
+    m1 = BooleanField()
+    m2 = BooleanField()
+    m3 = BooleanField()
+    m4 = BooleanField()
+    
+    t1 = BooleanField()
+    t2 = BooleanField()
+    t3 = BooleanField()
+    t4 = BooleanField()
+    
+    w1 = BooleanField()
+    w2 = BooleanField()
+    w3 = BooleanField()
+    w4 = BooleanField()
+    
+    th1 = BooleanField()
+    th2 = BooleanField()
+    th3 = BooleanField()
+    th4 = BooleanField()
+
+    f1 = BooleanField()
+    f2 = BooleanField()
+    f3 = BooleanField()
+    f4 = BooleanField()
+    
+    s1 = BooleanField()
+    s2 = BooleanField()
+    s3 = BooleanField()
+    s4 = BooleanField()
+    
+    su1 = BooleanField()
+    su2 = BooleanField()
+    su3 = BooleanField()
+    su4 = BooleanField()
 
     firstname = StringField('First Name', validators=[Length(min=2, max=20)], render_kw={"placeholder": "Enter first name"})
     lastname = StringField('Last Name', validators=[Length(min=2, max=20)], render_kw={"placeholder": "Enter last name"})
