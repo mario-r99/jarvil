55fb5e52fd03a29d6f2c09d40a54e2286f7d6c11
Mario <mario.srx99@gmail.com>
2021-05-21 16:11:04 +0200

    Added mqtt connection

--- brightness-service/brightness.py
+++ brightness-service/brightness.py
@@ -1,12 +1,28 @@
+import paho.mqtt.client as mqtt
 import pyfirmata
 import time
-if __name__ == '__main__':
-    board = pyfirmata.Arduino('/dev/ttyACM0')
-    board.analog[0].mode = pyfirmata.INPUT  
-    it = pyfirmata.util.Iterator(board)  
-    it.start()  
-    print("Communication Successfully started")
-    
-    while True:
-        print("Brightness:",board.analog[0].read())
-        time.sleep(1)
+
+# Global definitions
+broker_host = "192.168.178.100"
+mqtt_topic = "room/brightness"
+readout_frequency = 1
+
+# Client initialization
+client = mqtt.Client()
+
+# Connect to mqtt client and start loop
+client.connect(broker_host)
+client.loop_start()
+
+# Setup arduino connection
+board = pyfirmata.Arduino('/dev/ttyACM0')
+board.analog[0].mode = pyfirmata.INPUT  
+it = pyfirmata.util.Iterator(board)  
+it.start()
+
+# Sensor readout loop
+while True:
+    brightness = board.analog[0].read()
+    print("Publishing brightness:", brightness)
+    client.publish(mqtt_topic, brightness)
+    time.sleep(readout_frequency)
