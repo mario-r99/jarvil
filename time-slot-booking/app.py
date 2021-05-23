5ab25536057762a0ba7fe7e6ad75ac5412aa8b30
Mario <mario.srx99@gmail.com>
2021-05-23 23:51:13 +0200

    Added redis

--- time-slot-booking/app.py
+++ time-slot-booking/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import BookingForm
 from flask_mqtt import Mqtt
+import redis
 import json
+import time
+import sys
 
-# Initialize flask
+# Initialize flask server
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8fd4b5b09d72052a2ef9e200dc3a990e'
 app.config['MQTT_BROKER_URL'] = '192.168.178.17'
@@ -13,25 +16,20 @@ app.config['MQTT_PASSWORD'] = ''
 app.config['MQTT_KEEPALIVE'] = 60
 app.config['MQTT_TLS_ENABLED'] = False
 
-# Initialize mqtt
-mqtt = Mqtt(app)
+# Initialize redis database
+cache = redis.Redis(host='redis', port=6379, charset="utf-8", decode_responses=True)
 
-bookings = [
-    [10,10,9,3], # Monday
-    [10,0,9,3],  # Tuesday
-    [10,10,9,3], # Wednesday
-    [1,10,9,4],  # Thursday
-    [10,7,9,3],  # Friday
-    [10,10,9,3], # Saturday
-    [10,10,9,3]  # Sunday
-]
+# Initialize mqtt client
+mqtt = Mqtt(app)
 
 # Default page
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form = BookingForm()
+    bookings = get_bookings()
     if request.method == 'POST':
         if form.validate_on_submit():
+            set_bookings(form)
             mqtt.publish('booking/new', json.dumps(format_input_data(form)))
             flash(f'Reservation submitted for {form.firstname.data} {form.lastname.data}.', 'success')
             return redirect(url_for('home'))
@@ -39,6 +37,27 @@ def home():
             flash('Invalid input. Look below for more information.', 'danger')
     return render_template('home.html', bookings=bookings, form=form)
 
+def get_bookings():
+    weeks = []
+    for week in range(7):
+        slots = []
+        for slot in range(4):
+            value = cache.get(f'week-{week}-slot-{slot}')
+            if value is None:
+                value = 0
+            slots.append(10 - int(value))
+        weeks.append(slots)
+    return weeks
+
+def set_bookings(form):
+    bookings = format_table(form)
+    for booking in bookings:
+        try:
+            cache.incr(f'week-{booking.get("week")}-slot-{booking.get("slot")}')
+        except redis.exceptions.ConnectionError as exc:
+            raise exc
+
+# Returning formatted form data
 def format_input_data(form):
     return {
         "firstname": form.firstname.data,
@@ -47,7 +66,9 @@ def format_input_data(form):
         "bookings": format_table(form)
     }
 
+# Converting table form into booking list
 def format_table(form):
+    # Put all form data into 2-dimensional array
     table = [
         [form.m1.data, form.m2.data, form.m3.data, form.m4.data],
         [form.t1.data, form.t2.data, form.t3.data, form.t4.data],
@@ -57,13 +78,13 @@ def format_table(form):
         [form.s1.data, form.s2.data, form.s3.data, form.s4.data],
         [form.su1.data, form.su2.data, form.su3.data, form.su4.data],
     ]
+    # iterate over array and push booked entries on list
     bookings = []
     for week_index, week in enumerate(table):
         for slot_index, slot in enumerate(week):
             if slot == True:
                 bookings.append({
-                    "week": week_index+1,
-                    "slot": slot_index+1
+                    "week": week_index,
+                    "slot": slot_index
                 })
     return bookings
-
