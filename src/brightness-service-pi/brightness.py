4a8981a70404b6f654dea47efe3a9890122282c0
Christo JvV <83091592+Christo-JvV@users.noreply.github.com>
2021-06-02 16:32:00 +0200

    Added: brightness-service, mqtt-client, influxdb & grafana  (#12)
    * Added client
    
    * Separated docker compose files into dev and prod
    
    * Experimental/mqtt client (#7)
    
    * Added client
    
    * Separated docker compose files into dev and prod
    
    * Removed balena automation application path (#8)
    
    * Delete mqtt-client directory
    
    * Update docker-compose.yml
    
    * Update docker-compose-dev.yml
    
    * Added logging functionality
    
    * Added local connection
    
    * Added influxdb and grafana to docker compose.yml
    
    * Added brightness service
    
    * Added mqtt connection
    
    * update docker compose for influxdb v2.0
    
    * update for loging brightess data to influxdb via mqtt-brocker
    
    * lokal tests
    
    * changes for new file structure
    
    * fix: review comments 1
    
    Co-authored-by: Mario <mario.srx99@gmail.com>
    Co-authored-by: Mario R <48869294+mario-r99@users.noreply.github.com>
    Co-authored-by: Christo Janse van Vuuren <blank@blank.com>
    Co-authored-by: densalzmann <57320508+densalzmann@users.noreply.github.com>

---
+++ src/brightness-service-pi/brightness.py
@@ -0,0 +1,29 @@
+import paho.mqtt.client as mqtt
+import pyfirmata
+import time
+
+# Global definitions
+broker_host = "mqtt-broker"
+usb_port = '/dev/ttyACM0'
+mqtt_topic = "room/brightness"
+readout_frequency = 1
+
+# Client initialization
+client = mqtt.Client()
+
+# Connect to mqtt client and start loop
+client.connect(broker_host)
+client.loop_start()
+
+# Setup arduino connection
+board = pyfirmata.Arduino(usb_port)
+board.analog[0].mode = pyfirmata.INPUT  
+it = pyfirmata.util.Iterator(board)  
+it.start()
+
+# Sensor readout loop
+while True:
+    brightness = board.analog[0].read()
+    print("Publishing brightness:", brightness)
+    client.publish(mqtt_topic, brightness)
+    time.sleep(readout_frequency)
