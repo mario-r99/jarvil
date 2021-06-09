8042c23106ad515ffc3ccd1ee85bd5036758a567
densalzmann <57320508+densalzmann@users.noreply.github.com>
2021-06-09 23:46:17 +0200

    changed brightness-service to climate-service && add environmental variables

--- services/climate-service-pc/climate-service.py
+++ services/climate-service-pc/climate-service.py
@@ -1,12 +1,12 @@
 import paho.mqtt.client as mqtt
 import pyfirmata
 import time
-import os
 
 # Global definitions
-broker_host = os.environ['MQTT_HOST']
-usb_port = '/dev/ttyACM0'
-brightness_topic = "pi-3/environment-service/value/brightness/state"
+broker_host = "localhost"
+usb_port = 'COM6'
+brightness_port = 0
+brightness_topic = "pc/climate-service/value/brightness/state"
 readout_frequency = 1
 
 # Client initialization
@@ -18,13 +18,13 @@ client.loop_start()
 
 # Setup arduino connection
 board = pyfirmata.Arduino(usb_port)
-board.analog[0].mode = pyfirmata.INPUT  
+board.analog[brightness_port].mode = pyfirmata.INPUT  
 it = pyfirmata.util.Iterator(board)  
 it.start()
 
 # Sensor readout loop
 while True:
-    brightness = board.analog[0].read()
+    brightness = board.analog[brightness_port].read()
     print("Publishing brightness:", brightness)
     client.publish(brightness_topic, brightness)
     time.sleep(readout_frequency)
